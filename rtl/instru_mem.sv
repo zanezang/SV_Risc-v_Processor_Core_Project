74b7582fc1900fc5cdfffcc91ae7a098cb2dfa3a
zanezang <zane0698@gmail.com>
2026-08-09 20:28:43 -0400

    Comments Enhancement

--- rtl/instru_mem.sv
+++ rtl/instru_mem.sv
@@ -12,8 +12,6 @@ module instruction_mem (
     end
 
     // Word-addressing alignment layout: 
-    // Because the PC increments by 4 bytes per instruction, we drop the bottom 
-    // two bits (shift right by 2) to align the address with our array indexes.
     assign instruction = mem_array[pc_addr[31:2]];
 
 endmodule
